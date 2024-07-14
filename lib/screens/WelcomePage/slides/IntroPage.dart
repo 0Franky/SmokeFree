e4145463826f0177b63126b301ad594006973c98
0Franky <frhaexin@gmail.com>
2024-07-14 19:52:51 +0200

    update

--- lib/screens/WelcomePage/slides/IntroPage.dart
+++ lib/screens/WelcomePage/slides/IntroPage.dart
@@ -14,7 +14,7 @@ Widget buildIntroPage(final PageController pageController) {
           textAlign: TextAlign.center,
         ),
         SizedBox(height: 20),
-        Image.asset('images/logo.jpg', height: 200),
+        Image.asset('images/logo.png', height: 200),
         SizedBox(height: 20),
         Text(
           'L\'app che ti supporta nel tuo percorso per smettere di fumare. Inizia oggi stesso per migliorare la tua salute e il tuo futuro.',
